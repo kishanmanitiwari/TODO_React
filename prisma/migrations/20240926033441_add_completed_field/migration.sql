eeb4d77ddb77711df923ed63ebf97d790eddce9f
Prashant Jagtap <121176475+prashant2130@users.noreply.github.com>
2024-09-28 09:45:28 +0530

    Delete prisma directory

--- prisma/migrations/20240926033441_add_completed_field/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Todo" DROP CONSTRAINT "Todo_t_id_fkey";
-
--- AlterTable
-ALTER TABLE "Todo" ADD COLUMN     "completed" BOOLEAN NOT NULL DEFAULT false;
-
--- AddForeignKey
-ALTER TABLE "Todo" ADD CONSTRAINT "Todo_u_id_fkey" FOREIGN KEY ("u_id") REFERENCES "Users"("u_id") ON DELETE RESTRICT ON UPDATE CASCADE;
